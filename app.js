da6371e2797c7dab15b923e43d77815a39dfa208
Maksim Dav <maksim.dav@mail.ru>
2022-09-11 10:58:36 +0800

    install winston and add logger.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const DB_CONN = 'mongodb://localhost:27017/bitfilmsdb';
 
@@ -15,5 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect(DB_CONN, {
   useNewUrlParser: true,
 });
+
+app.use(requestLogger);
+
+app.use(errorLogger);
 
 app.listen(PORT);
